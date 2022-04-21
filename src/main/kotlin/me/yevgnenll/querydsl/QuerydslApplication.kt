c483105e2429743c6c90d20290f128c5ca2a10b7
yevgnenll <yevgnenll@gmail.com>
2022-04-21 11:13:46 +0900

    first commit

---
+++ src/main/kotlin/me/yevgnenll/querydsl/QuerydslApplication.kt
@@ -0,0 +1,11 @@
+package me.yevgnenll.querydsl
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuerydslApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuerydslApplication>(*args)
+}
---
+++ src/test/kotlin/me/yevgnenll/querydsl/QuerydslApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.yevgnenll.querydsl
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuerydslApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
